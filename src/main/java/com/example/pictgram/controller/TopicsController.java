a34edcbb88195cd7eb7ab22a4c36c94cb130ac62
hamuichiro <hamuichiro8616@icloud.com>
2022-04-28 10:19:45 +0900

    #36

--- src/main/java/com/example/pictgram/controller/TopicsController.java
+++ src/main/java/com/example/pictgram/controller/TopicsController.java
@@ -279,58 +279,7 @@ public class TopicsController {
     
     	return url;
     }
-    private void setGeoInfo(Topic entity, BufferedInputStream inputStream, String fileName)
-            throws ImageProcessingException, IOException, ImageReadException {
-        Metadata metadata = ImageMetadataReader.readMetadata(inputStream);
-        setGeoInfo(entity, metadata, inputStream, null, fileName);
-    }
 
-    private void setGeoInfo(Topic entity, File destFile, String fileName)
-            throws ImageProcessingException, IOException, ImageReadException {
-        Metadata metadata = ImageMetadataReader.readMetadata(destFile);
-        setGeoInfo(entity, metadata, null, destFile, fileName);
-    }
-
-    private void setGeoInfo(Topic entity, Metadata metadata, BufferedInputStream inputStream, File destFile,
-            String fileName) {
-        if (log.isDebugEnabled()) {
-            for (Directory directory : metadata.getDirectories()) {
-                for (Tag tag : directory.getTags()) {
-                    log.debug("{} {}", tag.toString(), tag.getTagType());
-                }
-            }
-        }
-
-        try {
-            IImageMetadata iMetadata = null;
-            if (inputStream != null) {
-                iMetadata = Sanselan.getMetadata(inputStream, fileName);
-                IOUtils.closeQuietly(inputStream);
-            }
-            if (destFile != null) {
-                iMetadata = Sanselan.getMetadata(destFile);
-            }
-            if (iMetadata != null) {
-                GPSInfo gpsInfo = null;
-                if (iMetadata instanceof JpegImageMetadata) {
-                    gpsInfo = ((JpegImageMetadata) iMetadata).getExif().getGPS();
-                    if (gpsInfo != null) {
-                        log.debug("latitude={}", gpsInfo.getLatitudeAsDegreesNorth());
-                        log.debug("longitude={}", gpsInfo.getLongitudeAsDegreesEast());
-                        entity.setLatitude(gpsInfo.getLatitudeAsDegreesNorth());
-                        entity.setLongitude(gpsInfo.getLongitudeAsDegreesEast());
-                    }
-                } else {
-                    List<?> items = iMetadata.getItems();
-                    for (Object item : items) {
-                        log.debug(item.toString());
-                    }
-                }
-            }
-        } catch (ImageReadException | IOException e) {
-            log.warn(e.getMessage(), e);
-        }
-    }
     
     @RequestMapping(value = "/topics/topic.csv", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE
             + "; charset=UTF-8; Content-Disposition: attachment")
@@ -345,7 +294,7 @@ public class TopicsController {
 
         return mapper.writer(schema).writeValueAsString(csv);
 
-
+    }
     private void setGeoInfo(Topic entity, BufferedInputStream inputStream, String fileName)
             throws ImageProcessingException, IOException, ImageReadException {
         Metadata metadata = ImageMetadataReader.readMetadata(inputStream);
@@ -399,5 +348,6 @@ public class TopicsController {
         }
 
     }
+    
 
 }
